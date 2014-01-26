a742be5436f7a1d13d30a30af4aea359cff15c2a
andrewmac <andrewmac@gmail.com>
2014-01-25 21:15:22 -0400

    Test/Example Project

---
+++ My Projects/testPhysX3/game/art/materials.cs
@@ -0,0 +1,10 @@
+
+singleton Material(newMaterial)
+{
+   mapTo = "unmapped_mat";
+   diffuseColor[0] = "0 0.768628 1 1";
+   diffuseMap[0] = "art/shapes/physx3/grid.png";
+   materialTag0 = "Miscellaneous";
+   specularStrength[0] = "0.294118";
+   pixelSpecular[0] = "1";
+};
