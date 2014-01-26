2dec05a734472605c7000ae9bf8517099c7c0817
andrewmac <andrewmac@gmail.com>
2014-01-25 22:35:31 -0400

    px3ClothPlane + Test ClothPlane level.

--- My Projects/testPhysX3/game/art/materials.cs
+++ My Projects/testPhysX3/game/art/materials.cs
@@ -8,3 +8,14 @@ singleton Material(newMaterial)
    specularStrength[0] = "0.294118";
    pixelSpecular[0] = "1";
 };
+
+singleton Material(newMaterial2)
+{
+   mapTo = "unmapped_mat";
+   diffuseColor[0] = "0.964706 0.964706 0.964706 1";
+   diffuseMap[0] = "art/shapes/physx3/fabric.png";
+   specularStrength[0] = "1.37255";
+   pixelSpecular[0] = "0";
+   materialTag0 = "Miscellaneous";
+   doubleSided = "1";
+};
